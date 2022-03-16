1d3dc74d4ebf7cef463f4eacd059eec1a731bb84
bensilverbento <100796237+bensilverbento@users.noreply.github.com>
2022-03-16 14:52:06 -0400

    Update BBRenamer.sh

--- BBRenamer.sh
+++ BBRenamer.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 # Ben Silver
 
-
-clear
-
+Clear
 Echo "====================================="
 Echo "=                                   ="
 Echo "=  Removing previous JAMF profiles  ="
 Echo "=                                   ="
 Echo "====================================="
 Echo ""
+
+
+# Remove JAMF if Installed
 sudo /usr/local/bin/jamf removeFramework -v foo >/dev/null 2>&1 || { echo "No Previous JAMF profiles found" >&2; }
 Echo ""
-read -p "Press Enter to Continue"
+read -p "Press Enter to Continue: "
 
 
 # Grab the serial number and rename the computer 
@@ -20,69 +21,83 @@ sn=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
 scutil --set LocalHostName BB$sn
 scutil --set ComputerName BB-$sn
 scutil --set HostName BB-$sn
-Clear
-Echo "================================="
-Echo "= Updating Computer Information ="
-Echo "================================="
-Echo "  ***"
-sleep 1
-clear
-Echo "================================="
-Echo "= Updating Computer Information ="
-Echo "================================="
-Echo "  ******"
-sleep 1
-Clear
-Echo "================================="
-Echo "= Updating Computer Information ="
-Echo "================================="
-Echo "  *********"
-sleep 1
-Clear
-Echo "================================="
-Echo "= Updating Computer Information ="
-Echo "================================="
-Echo "  ************"
-sleep 1
-clear
-Echo "================================="
-Echo "= Updating Computer Information ="
-Echo "================================="
-Echo "  ***************"
-sleep 1
-Clear
-Echo "================================="
-Echo "= Updating Computer Information ="
-Echo "================================="
-Echo "  ******************"
-sleep 1
-Clear
-Echo "================================="
-Echo "= Updating Computer Information ="
-Echo "================================="
-Echo "  *********************"
-sleep 1
-Clear
-Echo "================================="
-Echo "= Updating Computer Information ="
-Echo "================================="
-Echo "  ************************"
-sleep 1
-Clear
-Echo "================================="
-Echo "= Updating Computer Information ="
-Echo "================================="
-Echo "  ***************************"
-sleep 1
-clear
-Echo "================================="
-Echo "= Updating Computer Information ="
-Echo "================================="
-Echo "  ******************************"
 sudo dscacheutil -flushcache
-sleep 1
+
+# Just killing some time for the machine to refresh the names
 Clear
-Echo "Names:"
+Echo "=================================="
+Echo "= Updating Computer Information: ="
+Echo "=================================="
+Echo "--***--"
+Echo ""
+Sleep 1
+clear
+Echo "=================================="
+Echo "= Updating Computer Information: ="
+Echo "=================================="
+Echo "--******--"
+Echo ""
+Sleep 1
+Clear
+Echo "=================================="
+Echo "= Updating Computer Information: ="
+Echo "=================================="
+Echo "--*********--"
+Echo ""
+Sleep 1
+Clear
+Echo "=================================="
+Echo "= Updating Computer Information: ="
+Echo "=================================="
+Echo "--************--"
+Echo ""
+Sleep 1
+clear
+Echo "=================================="
+Echo "= Updating Computer Information: ="
+Echo "=================================="
+Echo "--***************--"
+Echo ""
+Sleep 1
+Clear
+Echo "=================================="
+Echo "= Updating Computer Information: ="
+Echo "=================================="
+Echo "--******************--"
+Echo ""
+Sleep 1
+Clear
+Echo "=================================="
+Echo "= Updating Computer Information: ="
+Echo "=================================="
+Echo "--*********************--"
+Echo ""
+Sleep 1
+Clear
+Echo "=================================="
+Echo "= Updating Computer Information: ="
+Echo "=================================="
+Echo "--************************--"
+Echo ""
+Sleep 1
+Clear
+Echo "=================================="
+Echo "= Updating Computer Information: ="
+Echo "=================================="
+Echo "--***************************--"
+Echo ""
+Sleep 1
+clear
+Echo "=================================="
+Echo "= Updating Computer Information: ="
+Echo "=================================="
+Echo "--******************************--"
+Echo ""
+Sleep 1
+Clear
+Echo "==============================="
+Echo "= Names (may require restart) ="
+Echo "==============================="
 Echo ""
 Echo "ComputerName: $(scutil --get ComputerName)"
 Echo "HostName: $(scutil --get HostName)"
