345d0e8d3fe879e737037e73920576307d6842fb
bensilverbento <100796237+bensilverbento@users.noreply.github.com>
2022-03-16 14:40:02 -0400

    Update BBRenamer.sh

--- BBRenamer.sh
+++ BBRenamer.sh
@@ -4,10 +4,14 @@
 
 clear
 
-Echo Removing previous JAMF profiles
-Echo
+Echo "====================================="
+Echo "=                                   ="
+Echo "=  Removing previous JAMF profiles  ="
+Echo "=                                   ="
+Echo "====================================="
+Echo ""
 sudo /usr/local/bin/jamf removeFramework -v foo >/dev/null 2>&1 || { echo "No Previous JAMF profiles found" >&2; }
-
+Echo ""
 read -p "Press Enter to Continue"
 
 
@@ -17,71 +21,71 @@ scutil --set LocalHostName BB$sn
 scutil --set ComputerName BB-$sn
 scutil --set HostName BB-$sn
 Clear
-Echo =================================
-Echo = Updating Computer Information =
-Echo =================================
+Echo "================================="
+Echo "= Updating Computer Information ="
+Echo "================================="
 Echo "  ***"
 sleep 1
 clear
-Echo =================================
-Echo = Updating Computer Information =
-Echo =================================
+Echo "================================="
+Echo "= Updating Computer Information ="
+Echo "================================="
 Echo "  ******"
 sleep 1
 Clear
-Echo =================================
-Echo = Updating Computer Information =
-Echo =================================
+Echo "================================="
+Echo "= Updating Computer Information ="
+Echo "================================="
 Echo "  *********"
 sleep 1
 Clear
-Echo =================================
-Echo = Updating Computer Information =
-Echo =================================
+Echo "================================="
+Echo "= Updating Computer Information ="
+Echo "================================="
 Echo "  ************"
 sleep 1
 clear
-Echo =================================
-Echo = Updating Computer Information =
-Echo =================================
+Echo "================================="
+Echo "= Updating Computer Information ="
+Echo "================================="
 Echo "  ***************"
 sleep 1
 Clear
-Echo =================================
-Echo = Updating Computer Information =
-Echo =================================
+Echo "================================="
+Echo "= Updating Computer Information ="
+Echo "================================="
 Echo "  ******************"
 sleep 1
 Clear
-Echo =================================
-Echo = Updating Computer Information =
-Echo =================================
+Echo "================================="
+Echo "= Updating Computer Information ="
+Echo "================================="
 Echo "  *********************"
 sleep 1
 Clear
-Echo =================================
-Echo = Updating Computer Information =
-Echo =================================
+Echo "================================="
+Echo "= Updating Computer Information ="
+Echo "================================="
 Echo "  ************************"
 sleep 1
 Clear
-Echo =================================
-Echo = Updating Computer Information =
-Echo =================================
+Echo "================================="
+Echo "= Updating Computer Information ="
+Echo "================================="
 Echo "  ***************************"
 sleep 1
 clear
-Echo =================================
-Echo = Updating Computer Information =
-Echo =================================
+Echo "================================="
+Echo "= Updating Computer Information ="
+Echo "================================="
 Echo "  ******************************"
 sudo dscacheutil -flushcache
 sleep 1
 Clear
-Echo Names:
-Echo
-Echo ComputerName: $(scutil --get ComputerName)
-Echo HostName: $(scutil --get HostName)
-Echo LocalHostName: $(scutil --get LocalHostName)
-Echo
-Echo
+Echo "Names:"
+Echo ""
+Echo "ComputerName: $(scutil --get ComputerName)"
+Echo "HostName: $(scutil --get HostName)"
+Echo "LocalHostName: $(scutil --get LocalHostName)"
+Echo ""
+Echo ""
