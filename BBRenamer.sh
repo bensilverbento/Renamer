44191d7a0fdebf73da687ea8c5f8add21cb5e57f
bensilverbento <100796237+bensilverbento@users.noreply.github.com>
2022-03-16 12:45:16 -0400

    Update BBRenamer.sh

--- BBRenamer.sh
+++ BBRenamer.sh
@@ -2,9 +2,9 @@
 # Ben Silver
 # Grab the serial number and rename the computer 
 sn=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
+scutil --set LocalHostName BB$sn
 scutil --set ComputerName BB-$sn
 scutil --set HostName BB-$sn
-scutil --set LocalHostName BB$sn
 Clear
 Echo Updating Computer Information:
 Echo ===
@@ -45,12 +45,12 @@ clear
 Echo Updating Computer Information:
 Echo ==============================
 sleep 1
-
+exec zsh -l
 Clear
 Echo Names:
 Echo
 Echo ComputerName: $(scutil --get ComputerName)
 Echo HostName: $(scutil --get HostName)
-Echo LocalHostName: $(scutil --get LocalHostName)
+Echo LocalHostName: $(BB-$sn)
 Echo
 Echo
