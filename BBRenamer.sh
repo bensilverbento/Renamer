fc0689f2dee0ceaba33a1513d72a438099ca867f
bensilverbento <100796237+bensilverbento@users.noreply.github.com>
2022-03-16 13:39:39 -0400

    Update BBRenamer.sh

--- BBRenamer.sh
+++ BBRenamer.sh
@@ -3,13 +3,14 @@
 
 
 clear
-
-Echo Removing previous JAMF profiles
+Echo ===================================
+Echo = Removing previous JAMF profiles =
+Echo ===================================
 Echo
 sudo /usr/local/bin/jamf removeFramework -v foo >/dev/null 2>&1 || { echo "No Previous JAMF profiles found" >&2; }
 
+Echo
 read -p "Press Enter to Continue"
-
 
 # Grab the serial number and rename the computer 
 sn=$(system_profiler SPHardwareDataType | awk '/Serial/ {print $4}')
